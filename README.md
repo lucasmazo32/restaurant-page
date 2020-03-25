f182308095f735bf326ebd8579a3164a9e12d17c
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-25 11:57:38 -0500

    started to create the tabs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # restaurant-page
 Project from microverse
+
+# Images
+
+Image by "restaurant" by 张 金锋 is licensed under CC BY-NC-ND 4.0 . You can check the image [here](https://search.creativecommons.org/photos/8d3fdf46-28b4-4457-9366-0fc9b854b6c2).
--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 import './style.scss';
+
+const tab1 = document.querySelector('.tab-1');
+const tab2 = document.querySelector('.tab-2');
+const tab3 = document.querySelector('.tab-3');
+
+// Title
+const title = document.createElement('h1');
+const titleText = document.createTextNode('Restaurant Project');
+title.appendChild(titleText);
+
+// Image
+const image = document.createElement('img');
+image.setAttribute('src', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a9fba334420899.56d68c8fa938f.jpg');
+image.setAttribute('alt', 'Restaurant');
+
+// Text
+const para = document.createElement('p');
+const paraText = document.createTextNode('We have been in the market for more than 100 years. We have a classic style and the same recipes from when we open for the first time. Take a moment to enjoy all of our plates; you will not be disappointed! If you have any suggestions or questions, feel free to ask us!');
+para.appendChild(paraText);
+
+// Joining them together
+tab1.appendChild(title);
+tab1.appendChild(image);
+tab1.appendChild(para);
