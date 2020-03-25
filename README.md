443c0c0048cc552f7938ea700a44f6ac6b6dc8b2
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-25 16:09:49 -0500

    started to work on the DOM and animations

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Project from microverse
 # Images
 
 Image by "restaurant" by 张 金锋 is licensed under CC BY-NC-ND 4.0 . You can check the image [here](https://search.creativecommons.org/photos/8d3fdf46-28b4-4457-9366-0fc9b854b6c2).
+
+"Coffee Menu Template" by Barcelona Design Shop is licensed under CC BY-NC-ND 4.0 . You can check the image [here](https://search.creativecommons.org/photos/4f36ef46-0a13-47d6-a579-fc9303fde26c).
+https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/2baa5d55105465.597700cb2f87e.jpg
--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import './style.scss';
 
-const tab1 = document.querySelector('.tab-1');
-const tab2 = document.querySelector('.tab-2');
-const tab3 = document.querySelector('.tab-3');
+const contentDiv = document.querySelector('.content');
+const tabs = document.querySelectorAll('.input-radio');
 
 // Title
-const title = document.createElement('h1');
-const titleText = document.createTextNode('Restaurant Project');
-title.appendChild(titleText);
+const titleHome = document.createElement('h1');
+const titleTextHome = document.createTextNode('Restaurant Project');
+titleHome.appendChild(titleTextHome);
 
 // Image
-const image = document.createElement('img');
-image.setAttribute('src', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a9fba334420899.56d68c8fa938f.jpg');
-image.setAttribute('alt', 'Restaurant');
+const imageHome = document.createElement('img');
+imageHome.setAttribute('src', 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/a9fba334420899.56d68c8fa938f.jpg');
+imageHome.setAttribute('alt', 'Restaurant');
 
 // Text
-const para = document.createElement('p');
-const paraText = document.createTextNode('We have been in the market for more than 100 years. We have a classic style and the same recipes from when we open for the first time. Take a moment to enjoy all of our plates; you will not be disappointed! If you have any suggestions or questions, feel free to ask us!');
-para.appendChild(paraText);
+const paraHome = document.createElement('p');
+const paraTextHome = document.createTextNode('We have been in the market for more than 100 years. We have a classic style and the same recipes from when we open for the first time. Take a moment to enjoy all of our plates; you will not be disappointed! If you have any suggestions or questions, feel free to ask us!');
+paraHome.appendChild(paraTextHome);
 
 // Joining them together
-tab1.appendChild(title);
-tab1.appendChild(image);
-tab1.appendChild(para);
+contentDiv.appendChild(titleHome);
+contentDiv.appendChild(imageHome);
+contentDiv.appendChild(paraHome);
