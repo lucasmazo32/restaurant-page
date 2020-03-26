9e775e1c094c6545a293de08b7ce5976449d6808
Lucas Mazo <lucasmazo32@gmail.com>
2020-03-26 16:44:02 -0500

    Create a new module to inject the code for the tabs and created a div for each time I inject the tab content.

--- src/contactContent.js
+++ src/contactContent.js
@@ -1,19 +1,18 @@
 const title = document.createElement('h1');
 const para = document.createElement('p');
+const contentDiv = document.createElement('div');
+contentDiv.setAttribute('class', 'content');
 
-const insertContact = (contentDiv, container) => {
+const insertContact = (container) => {
   // Title
-  title.innerHTML = 'Contact information';
+  title.textContent = 'Contact information';
 
   // Text
   para.innerHTML = 'Phone: 927.776.1537 x26493 <br> Email: Cecile80@gmail.com <br> Adress: 0583 Rau Ramp, Howellton';
 
   // Joining them together
-  const content = contentDiv;
-  content.innerHTML = '';
-  content.appendChild(title);
-  content.appendChild(para);
-  container.appendChild(content);
+  contentDiv.append(title, para);
+  container.appendChild(contentDiv);
 };
 
 export default insertContact;
--- src/insertContent.js
+++ src/insertContent.js
@@ -1,15 +1,15 @@
 import insertContact from './contactContent';
 import insertHome from './homeContent';
 import insertMenu from './menuContent';
+import insertTabs from './tabs';
 
-const tabs = document.querySelectorAll('.input-radio');
 const container = document.querySelector('.container');
-const contentDiv = document.createElement('div');
-contentDiv.setAttribute('class', 'content');
 
 const tabLogic = () => {
   let currentTab = 1;
-  insertHome(contentDiv, container);
+  insertTabs(container);
+  const tabs = document.querySelectorAll('.input-radio');
+  insertHome(container);
   tabs.forEach((element) => {
     const tab = element;
     tab.onclick = () => {
@@ -19,13 +19,13 @@ const tabLogic = () => {
         currentDiv.remove();
         switch (currentTab) {
           case 1:
-            insertHome(contentDiv, container);
+            insertHome(container);
             break;
           case 2:
-            insertMenu(contentDiv, container);
+            insertMenu(container);
             break;
           default:
-            insertContact(contentDiv, container);
+            insertContact(container);
             break;
         }
       }
